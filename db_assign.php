ce1a61cba6a585d974ffc09454e46e561be9fa4c
YusungLin <linyusung1998@gmail.com>
2021-05-09 15:36:26 -0400

    Change db password back to the default one

--- db_assign.php
+++ db_assign.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PWD', 'babysuse');
+define('DB_PWD', 'root');
 define('DB_NAME', 'vacc_db');
 class Scheduler {
     private $db_conn;
